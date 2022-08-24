b08f8a0fda1b43c5a7e62fdf9b97efaf02598f70
Sabelo Mdashe <selbysabelomdashe@gmail.com>
2022-08-24 12:05:45 +0200

    worked on the token and verify route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const middleware = require("../middleware/auth");
 
 // GETTING ALL USERS
 
-router.get("/", (req, res) => {
+router.get("/", middleware, (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
       if (err) throw err;
@@ -140,11 +142,45 @@ router.post("/login", (req, res) => {
         if (!matching) {
           res.send("Password Incorrect");
         } else {
-          res.send(result);
+          const payload = {
+            user: {
+              user_id: result[0].user_id,
+              full_name: result[0].full_name,
+              user_email: result[0].user_email,
+              user_type: result[0].user_type,
+            },
+          };
+
+          jwt.sign(
+            payload,
+            process.env.jwtSecret,
+            {
+              expiresIn: "365d",
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
         }
       }
     });
   } catch (error) {
     console.log(error);
   }
+});
+
+// VERIFY ROUTE
+
+router.get("/users/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({
+        msg: "Unauthorised Access!",
+      });
+    } else {
+      res.status(200).send(decodedToken);
+    }
+  });
 });
