6dda7b44acd86966568c0f356204c377701340be
Sabelo Mdashe <selbysabelomdashe@gmail.com>
2022-09-08 12:19:42 +0200

    fixing cors issue

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.use(cors());
 
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(_dirname + "/" + "index.html");
 });
